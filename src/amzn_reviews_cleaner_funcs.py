59271c5bd4b4de31cc2a159baab3a72849ae9f26
Rob Dalton <rob-dalton@users.noreply.github.com>
2016-11-15 14:08:07 -0800

    Added functions to remove punctuation and empty tokens

--- src/amzn_reviews_cleaner_funcs.py
+++ src/amzn_reviews_cleaner_funcs.py
@@ -12,13 +12,41 @@ import json
 import pandas as pd
 import numpy as np
 from pyspark.sql.functions import udf
+from pyspark.sql.types import ArrayType, StringType
 from pyspark.ml.feature import Tokenizer, CountVectorizer, StopWordsRemover, NGram, IDF
 from nltk.corpus import stopwords
+from string import maketrans
 
 # TFIDF helper functions
+def clean_reviewText(df):
+    # create translation table for punctuation
+    intab = '~!@#$%^&*()-_+=[]}{\|;:"<>,.?/'
+    outtab = '                              '
+    punc_tab = maketrans(intab, outtab)
+
+    # remove punctuation
+    punc_trans_udf = udf(lambda x: x.encode("utf-8").translate(punc_tab))
+    df_clean = df.withColumn("cleanText", punc_trans_udf(df["reviewText"]))
+
+    return df_clean
+
+
+def remove_empty_tokens(df):
+    remove_empty_udf = udf(lambda x: filter(None, x), ArrayType(StringType()))
+    df_raw_tokens_clean = df.withColumn("raw_tokens", remove_empty_udf(df["raw_tokens"]))
+
+    return df_raw_tokens_clean
+
+
 def tokenize(df):
-    tokenizer = Tokenizer(inputCol="reviewText", outputCol="raw_tokens")
+    # instantiate tokenizer
+    tokenizer = Tokenizer(inputCol="cleanText", outputCol="raw_tokens")
+
+    # create tokens
     df_raw_tokens = tokenizer.transform(df)
+
+    # remove empty tokens
+    #df_raw_tokens_clean = remove_empty_tokens(df_raw_tokens)
 
     return df_raw_tokens
 
@@ -49,8 +77,11 @@ def add_idf(df):
 
 
 def add_tfidf(df):
+    # clean
+    df_clean = clean_reviewText(df)
+
     # tokenize
-    df_raw_tokens = tokenize(df)
+    df_raw_tokens = tokenize(df_clean)
 
     # remove stopwords
     df_tokens = remove_stop_words(df_raw_tokens)
@@ -63,19 +94,8 @@ def add_tfidf(df):
 
     return df_idf, vocab
 
+
 # Extract features
-"""
-def extract_top_n_features(idf_vector, vocab, n):
-    # Get indices of top n features
-    # note - tfidf elements are pre-sorted by importance
-    term_indices = tfidf_vector.indices[-n:]
-
-    # Map features to terms
-    features = [vocab[i] for i in term_indices]
-
-    return features
-"""
-
 def add_top_features(df_tfidf, vocab, n=10):
 
     def extract_top_features(idf_vector, n):
